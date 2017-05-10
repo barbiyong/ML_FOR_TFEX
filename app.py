8db7eb389f9a96a5a1746fccb2c5541aaee2ff64
yong <barbiyong@hotmail.com>
2017-05-10 11:42:08 +0700

    make it better

--- app.py
+++ app.py
@@ -1,45 +1,27 @@
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi('UpXgIaRQWYXI36lC7IV4Uqwv2vD1zpA8VWHnXhgPZ/0cuI/qtnBvhB6j8G08+ooeBxGUm+S894UryM+Ana5nsf6SGq4HiiwT7qSvFxZk77XLpBXAvah0K+FsHTu0nlGweayyJ+NYCZXZVGQXKaqjVwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('1e0c49ab1c5adafb8d7c5d0750e89071')
+# -*- coding: utf-8 -*-
+from flask import Flask, request, render_template, jsonify
+app = Flask(__name__, static_url_path='')
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+@app.route('/', methods=['GET'])
+def main():
+    function_name = request.args.get('function_name')
+    if function_name == 'LP':
+        message = {
+            "messages": [
+                {"text": u" LAST PREDICT !! "}
+            ]
+        }
+        return jsonify(message)
+    else:
+        message = {
+            "messages": [
+                {"text": u" NOT HAVE THIS FEATURE ! "}
+            ]
+        }
+        return jsonify(message)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
